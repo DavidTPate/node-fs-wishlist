181eefee5aa1c4195a6f57fdf8b26924954990b4
David Pate <davidtpate@gmail.com>
2015-03-18 10:48:53 -0400

    Added the ability to recursively read a directory. Updated callback usage for `mkdirp` and `rmdirp`

---
+++ test/readdirp.js
@@ -0,0 +1,136 @@
+(function (chai, chaiAsPromised, dirtyChai, Promise, lib, fs, extend) {
+    'use strict';
+
+    if (!fs.existsAsync) {
+        fs = Promise.promisifyAll(fs);
+
+        fs.existsAsync = function (path) {
+            return new Promise(function (resolve, reject) {
+                fs.exists(path, function (exists) {
+                    resolve(exists);
+                });
+            });
+        };
+    }
+
+    chai.use(chaiAsPromised);
+    chai.use(dirtyChai);
+
+    var expect = chai.expect,
+        testFolder = 'test/mock';
+
+    describe('#readdirp', function () {
+        beforeEach(function () {
+            return fs.mkdirAsync(testFolder);
+        });
+        afterEach(function () {
+            return fs.rmdirAsync(testFolder);
+        });
+        it('should mixin readdirp by default', function () {
+            expect(lib.mixin(fs)).to.have.property('readdirp');
+        });
+        it('shouldn\'t mixin readdirp when already implemented', function () {
+            var readdirpFunc = function () {
+            };
+            expect(lib.mixin(extend({}, fs, {readdirp: readdirpFunc})).readdirp).to.equal(readdirpFunc);
+        });
+        it('should mixin readdirp when included', function () {
+            expect(lib.mixin(fs, {mixins: {readdirp: true}})).to.have.property('readdirp');
+        });
+        it('shouldn\'t mixin readdirp when excluded', function () {
+            expect(lib.mixin(fs, {mixins: {readdirp: false}})).to.not.have.property('readdirp');
+        });
+        it('should be able to recursively read directories', function () {
+            return fs.mkdirAsync(testFolder + '/one').then(function () {
+                return fs.mkdirAsync(testFolder + '/one/two').then(function () {
+                    return fs.mkdirAsync(testFolder + '/one/two/three').then(function () {
+                        return fs.mkdirAsync(testFolder + '/one/two/three/four');
+                    });
+                });
+            }).then(function () {
+                return Promise.all([
+                    fs.writeFileAsync(testFolder + '/1.txt', 'The impossible often has a kind of integrity to it which the merely improbable lacks.'),
+                    fs.writeFileAsync(testFolder + '/one/2.txt', 'It can be very dangerous to see things from somebody else\'s point of view without the proper training.'),
+                    fs.writeFileAsync(testFolder + '/one/two/3.txt', 'He was a dreamer, a thinker, a speculative philosopher... or, as his wife would have it, an idiot.'),
+                    fs.writeFileAsync(testFolder + '/one/two/three/4.txt', 'See first, think later, then test. But always see first. Otherwise you will only see what you were expecting. Most scientists forget that.'),
+                    fs.writeFileAsync(testFolder + '/one/two/three/four/5.txt', 'One is never alone with a rubber duck.'),
+                    fs.writeFileAsync(testFolder + '/one/two/three/four/6.txt', 'Anything that thinks logically can be fooled by something else that thinks at least as logically as it does.')
+                ]);
+            }).then(function () {
+                return lib.mixin(fs).readdirp(testFolder + '/one');
+            }).then(function (files) {
+                expect(files).to.deep.equal([
+                    testFolder + '/one',
+                    testFolder + '/one/2.txt',
+                    testFolder + '/one/two',
+                    testFolder + '/one/two/3.txt',
+                    testFolder + '/one/two/three',
+                    testFolder + '/one/two/three/4.txt',
+                    testFolder + '/one/two/three/four',
+                    testFolder + '/one/two/three/four/5.txt',
+                    testFolder + '/one/two/three/four/6.txt'
+                ]);
+                return true;
+            }).finally(function () {
+                return Promise.all([
+                    fs.unlinkAsync(testFolder + '/1.txt'),
+                    fs.unlinkAsync(testFolder + '/one/2.txt'),
+                    fs.unlinkAsync(testFolder + '/one/two/3.txt'),
+                    fs.unlinkAsync(testFolder + '/one/two/three/4.txt'),
+                    fs.unlinkAsync(testFolder + '/one/two/three/four/5.txt'),
+                    fs.unlinkAsync(testFolder + '/one/two/three/four/6.txt')
+                ]).then(function () {
+                    return fs.rmdirAsync(testFolder + '/one/two/three/four').then(function () {
+                        return fs.rmdirAsync(testFolder + '/one/two/three').then(function () {
+                            return fs.rmdirAsync(testFolder + '/one/two').then(function () {
+                                return fs.rmdirAsync(testFolder + '/one');
+                            });
+                        });
+                    });
+                });
+            });
+        });
+        it('should be able to recursively read directories with a callback', function () {
+            return fs.mkdirAsync(testFolder + '/one').then(function () {
+                return new Promise(function (resolve, reject) {
+                    lib.mixin(fs).readdirp(testFolder + '/one', function (err, files) {
+                        if (err) {
+                            return reject(err);
+                        }
+                        expect(files).to.deep.equal([testFolder + '/one']);
+                        resolve();
+                    });
+                });
+            }).finally(function () {
+                return fs.rmdirAsync(testFolder + '/one');
+            });
+        });
+        it('shouldn\'t be able to read a directory that already exists and isn\'t a directory', function () {
+            return fs.writeFileAsync(testFolder + '/one', 'The ships hung in the sky in much the same way bricks don\'t.').then(function () {
+                return expect(lib.mixin(fs).readdirp(testFolder + '/one')).to.eventually.be.rejectedWith(Error, /^ENOTDIR/);
+            }).finally(function () {
+                return fs.unlink(testFolder + '/one');
+            });
+        });
+        it('shouldn\'t be able to read a directory that doesn\'t exist', function () {
+            return expect(lib.mixin(fs).readdirp(testFolder + '/one')).to.eventually.be.rejectedWith(Error, /^ENOENT/);
+        });
+        it('should propagate an error from a stats call', function () {
+            var xfs = extend({}, lib.mixin(fs), {
+                stat: function (dir, cb) {
+                    cb(new Error('Some Stats Error'));
+                }
+            });
+            return Promise.all([
+                fs.mkdirAsync(testFolder + '/one'),
+                fs.writeFileAsync(testFolder + '/one/1.txt', 'In an infinite Universe anything can happen.')
+            ]).then(function () {
+                return expect(xfs.readdirp(testFolder + '/one')).to.eventually.be.rejectedWith(Error, 'Some Stats Error');
+            }).finally(function () {
+                return fs.unlinkAsync(testFolder + '/one/1.txt').then(function () {
+                    return fs.rmdir(testFolder + '/one');
+                });
+            });
+        });
+    });
+}(require('chai'), require('chai-as-promised'), require('dirty-chai'), require('bluebird'), require('../index'), require('fs'), require('extend')));
